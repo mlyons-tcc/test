b352b9131b9fb203b9c58c02ede274976a05dfc9
Mike Lyons <mike.lyons@climate.com>
2015-04-08 12:15:14 -0500

    add some more math

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 __author__ = 'mike.lyons'
 
 print "Hello World!"
+x = raw_input("Enter any number: ")
+y = raw_input("Enter another number: ")
 
-print 1+1
+try:
+    x = float(x)
+    y = float(y)
+except ValueError:
+    x = 0.0
+    y = 0.0
+
+print x+y
+print x/2
+print y**2
