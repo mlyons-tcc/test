fc6ad89460dca9e1e5b1a4effe14c868cafc0e54
Mike Lyons <mike.lyons@climate.com>
2015-04-08 12:50:11 -0500

    add looping around math

--- test.py
+++ test.py
@@ -1,16 +1,23 @@
 __author__ = 'mike.lyons'
 
 print "Hello World!"
-x = raw_input("Enter any number: ")
-y = raw_input("Enter another number: ")
 
-try:
-    x = float(x)
-    y = float(y)
-except ValueError:
-    x = 0.0
-    y = 0.0
+while True:
+    x = raw_input("Enter any number: ")
+    y = raw_input("Enter another number: ")
 
-print x+y
-print x/2
-print y**2
+    try:
+        x = float(x)
+        y = float(y)
+    except ValueError:
+        x = 0.0
+        y = 0.0
+
+    print x+y
+    print x/2
+    print y**2
+
+    user_exit = raw_input("Exit? (y/n): ")
+    if user_exit == 'y' or user_exit == 'Y':
+        break
+print "Goodbye World!"
